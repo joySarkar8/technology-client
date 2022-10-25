ddca28881311fe047bc3c9f6873f0d88987b744b
joySarkar8 <sarkarjoy498@gmail.com>
2022-10-25 22:40:47 +0600

    initialy setup router and header

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../layout/Main';
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>
+    }
+])
+
+
